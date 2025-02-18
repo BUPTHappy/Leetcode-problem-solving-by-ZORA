b7a0fdc60f47ad34747cd2d26b617c262a7e3e1a
Zhuoer Liu <zhuoer@kth.se>
2025-02-18 23:36:38 +0100

    2.18 solution

---
+++ 2. AddTwoNumbers.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+  {
+    ListNode *dummyHeader = new ListNode(0);
+    ListNode *current = dummyHeader;
+    int carry = 0; // 进位
+
+    while (l1 != NULL || l2 != NULL || carry != 0)
+    {
+      int sum = carry;
+      if (l1 != NULL)
+      {
+        sum += l1->val;
+        l1 = l1->next;
+      }
+      if (l2 != NULL)
+      {
+        sum += l2->val;
+        l2 = l2->next;
+      }
+
+      carry = sum / 10; // 更新进位
+      current->next = new ListNode(sum % 10);
+      current = current->next;
+    }
+    return dummyHeader->next;
+  }
+};

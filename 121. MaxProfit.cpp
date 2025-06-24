e4027d8be801d9498cce148f27896397246f5d2b
Zhuoer Liu <zhuoer@kth.se>
2025-06-24 19:34:33 +0200

    121_6.24

---
+++ 121. MaxProfit.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Zhuoer LIU on 2025-06-24.
+//
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=INT_MAX;
+        int maxProfit=0;
+        for(int price:prices){
+            if(price <min){
+                min=price;
+            }else {
+                maxProfit = max(maxProfit, price-min);
+            }
+        }
+        return maxProfit;
+    }
+};

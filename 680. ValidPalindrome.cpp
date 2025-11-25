b030c3b1ebfe24d4c1ba835c1fb33b5435df8b91
Zhuoer Liu <zhuoer@kth.se>
2025-11-25 23:51:01 +0100

    680_11.25

---
+++ 680. ValidPalindrome.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Zhuoer LIU on 2025-11-25.
+//
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left =0, right=s.size()-1;
+
+        while(left<right){
+            if(s[left]==s[right]){
+                left++;
+                right--;
+            }else{
+                return isPalindrome(s, left, right-1) || isPalindrome(s, left+1, right); //主函数中遇到不匹配时调用两次辅助函数
+            }
+        }
+        return true;
+    }
+
+    bool isPalindrome(const string& s, int left, int right){
+        for(int i=left, j=right; i<j; i++, j--){
+            if(s[i]!=s[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+};

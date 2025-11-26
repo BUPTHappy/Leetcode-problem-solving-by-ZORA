d6ce6dae77964d13b44376dc0bda601d8fa0e355
Zhuoer Liu <zhuoer@kth.se>
2025-11-26 20:29:56 +0100

    860_11.26

---
+++ 860. LemonadeChange.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Zhuoer LIU on 2025-11-26.
+//
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten=0;
+        for (int i=0; i<bills.size();i++){
+            if (bills[i]==5){
+                five++;
+            }else if(bills[i]==10){
+                if(five==0) return false;
+                five--;
+                ten++;
+            }else if(bills[i]==20){
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }else if(five>=3){
+                    five-=3;
+                }else return false;
+            }
+        }
+        return true;
+
+    }
+};

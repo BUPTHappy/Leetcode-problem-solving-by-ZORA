1ddcacb38b00d2b85074c7e57f9b38bdeb2cb5a1
Zhuoer Liu <zhuoer@kth.se>
2025-12-17 00:04:10 +0100

    231_12.17

---
+++ 231. IsPowerOfTwo.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Zhuoer LIU on 2025-12-17.
+//
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n>0 && n == (n & -n); //这里用的就是brain kernighan算法找到最右侧的1
+        //一个核心基础就是2的幂的数二进制都只会有一个1
+    }
+};

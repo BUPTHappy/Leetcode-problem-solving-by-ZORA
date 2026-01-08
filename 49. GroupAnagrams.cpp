18775fcfcb1f874f628a4bad084d8f9550fbc8af
Zhuoer Liu <zhuoer@kth.se>
2026-01-09 01:50:22 +0800

    49_1.9

---
+++ 49. GroupAnagrams.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<vector<string>> groupAnagrams(vector<string> &strs)
+  {
+    unordered_map<string, vector<string>> groups;
+    for (string &s : strs)
+    {
+      string key = s;
+      sort(key.begin(), key.end());
+      groups[key].push_back(s);
+    }
+
+    vector<vector<string>> ans;
+    for (auto &pair : groups)
+    {
+      ans.push_back(pair.second);
+    }
+    return ans;
+  }
+};

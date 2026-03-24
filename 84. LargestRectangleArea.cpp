71997e7920d0cebb0482d83e70a1f8d051418032
Zhuoer Liu <zhuoer@kth.se>
2026-03-24 22:46:45 +0100

    84_3.24

---
+++ 84. LargestRectangleArea.cpp
@@ -0,0 +1,26 @@
+#include<stack>
+using namespace std;
+
+class Solution
+{
+public:
+  int largestRectangleArea(vector<int> &heights)
+  {
+    stack<int> st;
+    int maxArea = 0;
+    heights.push_back(0);
+
+    for (int i = 0; i < heights.size(); i++)
+    {
+      while (!st.empty() && heights[i] < heights[st.top()])
+      {
+        int h = heights[st.top()];
+        st.pop();
+        int w = st.empty() ? i : i - st.top() - 1;
+        maxArea = max(h * w, maxArea);
+      }
+      st.push(i);
+    }
+    return maxArea;
+  }
+};

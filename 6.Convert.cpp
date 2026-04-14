592144b39892913a43f7ccc6c6084de85c57e265
Zhuoer Liu <zhuoer@kth.se>
2026-04-14 12:29:01 +0200

    6_4.14

---
+++ 6.Convert.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+  string convert(string s, int numRows)
+  {
+    if (numRows == 1)
+      return s;
+
+    // 创建numRows个字符串，分别存储每一行的字符
+    vector<string> rows(min(numRows, int(s.size())));
+    int curRow = 0;
+    bool goingDown = false;
+
+    for (char c : s)
+    {
+      rows[curRow] += c;
+
+      // z字形：到达最上或者最下就改变方向
+      if (curRow == 0 || curRow == numRows - 1)
+        goingDown = !goingDown;
+      curRow += goingDown ? 1 : -1;
+    }
+
+    string res;
+    for (string row : rows)
+      res += row;
+    return res;
+  }
+};
